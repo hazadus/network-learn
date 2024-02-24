3674fd5df79d42c03d34c6bc1e86663f72ec9971
hazadus <hazadus7@gmail.com>
2024-02-24 13:07:20 +0300

    feat(http_server): Basic request parser implemented.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# network-learn
+# 🕸️🖥️ network-learn
 
 В этом репозитории на практике изучаю работу с сетью.
 
@@ -9,8 +9,15 @@
 - Простейший эхосервер: [sockets/echo_server.py](sockets/echo_server.py)
 - Простейший клиент для эхосервера: [sockets/basic_client.py](sockets/basic_client.py)
 
+## HTTP
+
+Изучаем работу протокола HTTP на практике.
+
+- Простейший HTTP-сервер с синхронной обработкой запросов: [http/http_server.py](http/http_server.py)
+
 ## References
 
+- [`socket` — Low-level networking interface](https://docs.python.org/3/library/socket.html)
 - [Introducing The Sockets API](https://beej.us/guide/bgnet0/html/split/introducing-the-sockets-api.html#introducing-the-sockets-api)
 - [Beej’s Guide to Network Programming](https://beej.us/guide/bgnet) – optional, for C devs.
 - [Socket Programming HOWTO](https://docs.python.org/3.12/howto/sockets.html) in Python 3.12 documentation.
--- http/http_server.py
+++ http/http_server.py
@@ -3,6 +3,34 @@ Very simple HTTP server.
 """
 
 import socket
+from datetime import datetime
+from io import BufferedReader
+from typing import Tuple
+
+DEFAULT_HOST = "0.0.0.0"
+DEFAULT_PORT = 8000
+SUPPORTED_VERSIONS = [
+    "HTTP/1.1",
+]
+
+
+class Request:
+    def __init__(
+        self,
+        method: str,
+        target: str,
+        version: str,
+        request_file: BufferedReader,
+        client_address: Tuple[str, int],
+    ):
+        self.method = method
+        self.target = target
+        self.version = version
+        self.request_file = request_file
+        self.client_host, self.client_port = client_address
+
+    def __str__(self):
+        return f"<Request: {self.client_host}:{self.client_port} {self.method} {self.target} {self.version}>"
 
 
 def serve_forever(host: str, port: int):
@@ -11,21 +39,24 @@ def serve_forever(host: str, port: int):
     try:
         server_socket.bind((host, port))
         server_socket.listen()
+        print(f"HTTP server waiting for connection on {host}:{port}")
 
         while True:
-            connection, _ = server_socket.accept()
+            connection, address = server_socket.accept()
+            print(datetime.now(), f"{address[0]}:{address[1]} connected...")
 
             try:
-                serve_client(connection)
+                serve_client(connection, address)
             except Exception as ex:
                 print("Failed to serve client", ex)
     finally:
         server_socket.close()
 
 
-def serve_client(connection: socket.socket):
+def serve_client(connection: socket.socket, client_address: Tuple[str, int]):
     try:
-        request = parse_request(connection)
+        request = parse_request(connection, client_address)
+        print(datetime.now(), request)
         response = handle_request(request)
         send_response(connection, response)
     except ConnectionResetError:
@@ -37,7 +68,34 @@ def serve_client(connection: socket.socket):
         connection.close()
 
 
-def parse_request(connection: socket.socket): ...
+def parse_request(
+    connection: socket.socket,
+    client_address: Tuple[str, int],
+) -> Request:
+    """
+    Parse all neded data from request message into `Request` instance.
+    References:
+     - https://docs.python.org/3/library/socket.html#socket.socket.makefile
+     - https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+    :param connection: client connection socket
+    :param client_address: tuple of client host and port
+    :return: filled `Request` instance
+    """
+    # Represent connection as file-like object:
+    request_file = connection.makefile("rb")
+    line_bytes = request_file.readline()
+    request_line = str(line_bytes, "iso-8859-1").rstrip("\r\n")
+    tokens = request_line.split(" ")
+
+    # Expect exactly three tokens in request line - method, target, version:
+    if len(tokens) != 3:
+        raise Exception(f"Wrong request line: {request_line}")
+
+    method, target, version = tokens
+    if version not in SUPPORTED_VERSIONS:
+        raise Exception(f"Unsupported HTTP version: {version}")
+
+    return Request(method, target, version, request_file, client_address)
 
 
 def handle_request(request): ...
@@ -51,6 +109,6 @@ def send_error(connection: socket.socket, error): ...
 
 if __name__ == "__main__":
     try:
-        serve_forever("0.0.0.0", 8000)
+        serve_forever(DEFAULT_HOST, DEFAULT_PORT)
     except KeyboardInterrupt:
         ...
