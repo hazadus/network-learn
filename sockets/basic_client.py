70123447463e08b6915322cde4434ec5e395d818
hazadus <hazadus7@gmail.com>
2024-02-24 13:06:54 +0300

    refactor: isort + black.

--- sockets/basic_client.py
+++ sockets/basic_client.py
@@ -1,6 +1,7 @@
 """
 Very simple client to test with echo_server.py.
 """
+
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- sockets/echo_server.py
+++ sockets/echo_server.py
@@ -2,11 +2,14 @@
 Simple echo server.
 Run `telnet 127.0.0.1 53210` to connect.
 """
+
 import socket
 
-server_socket = socket.socket(socket.AF_INET,  # Create network socket
-                              socket.SOCK_STREAM,  # Use TCP (SOCK_DGRAM for UDP)  
-                              proto=0)  # Use IP protocol
+server_socket = socket.socket(
+    socket.AF_INET,  # Create network socket
+    socket.SOCK_STREAM,  # Use TCP (SOCK_DGRAM for UDP)
+    proto=0,  # Use IP protocol
+)
 # Make server socket accessible via all machine addresses (not only internal):
 server_socket.bind(("", 53210))
 # 10 is the max number of connected clients, awaiting response (backlog):
